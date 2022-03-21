4d802da50e0d26070141c57781f3a0d34251f351
Ston14 <alexis.debrito@ecole2600.com>
2022-03-21 19:29:06 +0100

    Memory.h is ok
    Reste plus qu'a coder

---
+++ header/my_memmap.h
@@ -0,0 +1,34 @@
+
+#include <stddef.h>
+
+#ifdef __linux__
+#define linux_archi
+#endif
+
+int getback_key_metadata(void);
+//Map la memoire
+void *my_memomap(size_t size);
+//memory map is fixed ?
+
+//memory mapp is protected ?
+bool my_mmap_fixed(void *ptr, size_t size);
+bool my_map_is_protect_by_ro(void *ptr, size_t size);
+bool my_map_is_protect_by_rw(void *ptr, size_t size);
+bool my_map_is_protect_by_rw_on_metadata(void *ptr, size_t size);
+
+
+//unmap memory
+bool my_unmap_mem(void *ptr, size_t size);
+
+//memory purge
+bool my_purge_mem(void *ptr, size_t size);
+bool name_my_memory(void *ptr, size_t size, const char* name);
+
+
+/*LINUX mremap compatbile :D Permet de remapper une interval d'adresse */
+#ifdef linux_archi
+
+bool my_memraping();
+bool my_memreaping_fixed();
+#endif
+
